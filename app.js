610fda378d0b270294d795a97648bde31f6f3dbe
William Hou <williamhou38@yahoo.com>
2022-09-16 15:49:50 -0500

    new changes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function part2(input1, input2) {
     let tf = 'FALSE';
-    let inputD = '';
     if(input1.toLowerCase().includes(input2)
         || input1.toUpperCase().includes(input2)
         || input1.includes(input2)) {
         tf = 'TRUE';
     } else {
+        let inputD = '';
         for(let i = 0; i < input1.length; i++) {
             inputD = input1.replace(input1.charAt(i), '');
             if(inputD.toLowerCase().includes(input2)
