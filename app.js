9463e107ef13c280c409c8b36777a689723427e1
William Hou <williamhou38@yahoo.com>
2022-09-16 14:56:09 -0500

    new changes

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 function part2(input1, input2) {
-    if(input1.toLowerCase().includes(input2) || input1.toUpperCase().includes(input2) || input1.includes(input2)) {
-        return 'TRUE'
+    let tf = 'FALSE';
+    let inputD = '';
+    if(input1.toLowerCase().includes(input2)
+        || input1.toUpperCase().includes(input2)
+        || input1.includes(input2)) {
+        tf = 'TRUE';
     } else {
-        return 'FALSE'
+        for(let i = 0; i < input1.length; i++) {
+            inputD = input1.replace(input1.charAt(i), '');
+            if(inputD.toLowerCase().includes(input2)
+                || inputD.toUpperCase().includes(input2)
+                || inputD.includes(input2)) {
+                tf = 'TRUE';
+            }
+        }
     }
+    return tf;
 }
 
-console.log('part2(): ', part2('BcD', 'BcD'));
+console.log('part2(ebCde, BCD): ', part2('ebCde', 'BCD'));
+console.log('part2(eCB, ECB): ', part2('eCB', 'ECB'));
+console.log('part2(BcdEF, BCEF): ', part2('BcdEF', 'BCEF'));
+console.log('part2(BCD, BcD): ', part2('BCD', 'BcD'));
